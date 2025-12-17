33891feff9be2e108254491889a45858280d7a3b
Rabith Ahmed <106297557+msrabith@users.noreply.github.com>
2025-12-17 20:28:58 +0530

    Improve error handling in extract_text_from_image
    Added error handling for image processing in OCR function.

--- ocr.py
+++ ocr.py
@@ -4,9 +4,13 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def extract_text_from_image(image_path):
-    img = Image.open(image_path)
-    text = pytesseract.image_to_string(img)
-    return text.strip()
+    try: 
+        img = Image.open(image_path.stream)
+        text = pytesseract.image_to_string(img)
+        return text.strip()
+    except Exception as e:
+        print("OCR ERROR:",e)
+        return "Could not read text from image. Please try a clearer image."
     
 def clean_ocr_text(text):
     text = text.replace("\n"," ")
