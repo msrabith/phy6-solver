80c0fd205f447972b917b9c0fa0d15efb26209ac
Rabith Ahmed <106297557+msrabith@users.noreply.github.com>
2025-12-17 22:18:45 +0530

    Conditionally set tesseract_cmd for Windows
    Set tesseract_cmd conditionally for Windows OS.

--- ocr.py
+++ ocr.py
@@ -1,9 +1,10 @@
 from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = (
-        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    )
+if os.name == "nt":
+        pytesseract.pytesseract.tesseract_cmd = (
+                r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+            )
 
 def extract_text_from_image(image_file):
     try: 
